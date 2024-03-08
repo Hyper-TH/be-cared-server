1efc0565fc603bda47dd1d0732b9730a69f36029
Twila [Hyper] Habab <twila.shayne.habab@outlook.com>
2024-03-08 22:21:47 +0000

    IE to US, cleanup

--- merck/methods.js
+++ merck/methods.js
@@ -21,7 +21,7 @@ export async function requestList(cookie, prodQuery, type) {
 	// Turn to all lower case for first instance of the prodQuery
     const option = {
         host: "www.sigmaaldrich.com",
-        path: `/US/en/search/${prodQuery.toLowerCase()}?focus=products&page=1&perpage=30&sort=relevance&term=${encodeURIComponent(prodQuery)}&type=product_${type}`,
+        path: `/US/en/search/${encodeURIComponent(prodQuery.toLowerCase())}?focus=products&page=1&perpage=30&sort=relevance&term=${encodeURIComponent(prodQuery)}&type=product_${type}`,
         headers: {
             accept: "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
 			accept_language: "en-US,en;q=0.9",
@@ -35,7 +35,7 @@ export async function requestList(cookie, prodQuery, type) {
 			service_worker_navigation_preload: "true",
 			upgrade_insecure_requests: "1",
 			cookie: `${cookie}`,
-			Referer: "https://www.sigmaaldrich.com/IE/en/search",
+			Referer: "https://www.sigmaaldrich.com/US/en/search",
 			ReferrerPolicy: "strict-origin-when-cross-origin",
         },
 		method: "GET",
@@ -46,11 +46,7 @@ export async function requestList(cookie, prodQuery, type) {
     return new Promise((resolve, reject) => {
         https.get(option, (response) => {
 			console.log(response.statusCode); // Logs the HTTP status code
-            
-			if (response.statusCode === 302) {
-				console.log('\nHeaders:\n', response.headers);
-			};
-
+        
 			let result = '';
 
             response.on('data', function (chunk) {
@@ -58,7 +54,7 @@ export async function requestList(cookie, prodQuery, type) {
             });
 
             response.on('end', function () {
-				const doc = result;
+				// const doc = result;
 				// const filePath = 'searchResults.html';
 
 				// // Write HTML content to file
