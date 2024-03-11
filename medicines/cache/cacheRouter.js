8efd3801316703e1d665f4719d5c744b9995d7e3
Twila [Hyper] Habab <twila.shayne.habab@outlook.com>
2024-03-11 02:23:35 +0000

    Moved caching endpoints

--- medicines/cache/cacheRouter.js
+++ medicines/cache/cacheRouter.js
@@ -5,8 +5,101 @@ import { firestore } from '../../config/config.js';
 import { tokenOptions } from '../tokenOptions.js';
 import { requestToken, requestDocument } from '../methods.js';
 
-
 const router = express.Router();
+
+// Endpoint to cache medicine
+router.get('/cacheMed', async (req, res) => {
+    try {
+        const { id, name, activeIngredient, company, status, pil, spc } = req.query;
+        console.log("ID:", id);
+
+        const documentID = id;
+        const collectionName = "medicines";
+
+        let documentSnapshot = await firestore.collection(collectionName).doc(documentID).get();
+
+        if (!documentSnapshot.exists) {
+            console.log(`Caching to server with new documentID: ${documentID}`);
+
+            const data = {
+                name: name,
+                activeIngredient: activeIngredient,
+                company: company,
+                status: status,
+                pilPath: pil,
+                spcPath: spc
+            };
+
+            try {
+                await firestore.collection(collectionName).doc(documentID).set(data);
+                
+                console.log("Cached to server!");
+            } catch (error) {
+
+                console.error("An error occurred at /cacheMed:", error);
+            } 
+
+        } else {
+            console.log(`Medicine is already cached`);
+        }
+
+    } catch (error) {
+        console.error("An error occured at /cacheMed:", error);
+    }
+});
+
+// Endpoint to get cache document
+router.get('/grabCache', async (req, res) => {
+    const { uploadPath } = req.query;
+
+    const documentID = uploadPath;
+    const collectionName = "files"; 
+    
+    try {
+        let documentSnapshot = await firestore.collection(collectionName).doc(documentID).get();
+        
+        // If it's already in the server (cached)
+        if (documentSnapshot.exists) {
+            console.log(`Found cached document`);
+            const documentData = documentSnapshot.data();
+            
+            res.type('application/pdf').send(documentData);
+        } else {
+            // If it does not, cache this to the server!
+            console.log(`Caching to server with new documentID: ${documentID}`);
+
+            const token = await requestToken(tokenOptions);
+            const document = await requestDocument(token, uploadPath);
+ 
+            const data = {
+                doc: document
+            }
+
+            // Sample medicine: PIL for Panadol ActiFast 500mg Soluble Tablets
+            try {
+                await firestore.collection(collectionName).doc(documentID).set(data);
+                
+                console.log("Cached to server!");
+              
+                let documentSnapshot = await firestore.collection(collectionName).doc(documentID).get();
+                const documentData = documentSnapshot.data();
+              
+                res.type('application/pdf').send(documentData);
+
+            } catch (error) {
+                console.error("An error occurred:", error);
+                
+                res.type('application/pdf').send(data);
+            } 
+        }
+
+    } catch (error) {
+      console.error(`Error fetching data: ${error}`);
+    }
+});
+
+
+// Endpoint
 
 /* START BLOCK THAT RUNS EVERY FRIDAY */
 // RUN EVERYDAY AT 12 PM
@@ -77,7 +170,9 @@ const weeklyCache = async () => {
                 const isEqual = Buffer.compare(newDocumentBuffer, cachedDocumentBuffer) === 0;
 
                 /*
-                    TODO: First grab the name of the medicine and execute a requestList() 
+                    TODO: 
+                    Caching medicine
+                    First grab the name of the medicine and execute a requestList() 
                     and take the first instance (i.e., response[0]) then take these:
                     name: medicine.name
                     id: medicine.id
@@ -152,6 +247,9 @@ const weeklyCache = async () => {
     
 };  
 
+// TODO: Move cache endpoints to here
+
+// TODO: Vercel has a setup for cron jobs, establish that
 const job = cron.schedule('0 12 * * *', weeklyCache, {
     scheduled: true,
     timezone: "Europe/London"
