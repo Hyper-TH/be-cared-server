fabd9d93f05e07df42ed218fdc458e1059b4d307
Twila [Hyper] Habab <twila.shayne.habab@outlook.com>
2024-03-24 00:28:28 +0000

    PDF Response diag and cleanup

--- server.js
+++ server.js
@@ -13,6 +13,11 @@ import cacheRouter from './medicines/cache/cacheRouter.js';
 const app = express();
 app.use(cors());
 app.use(express.json());
+// Increase JSON payload limit
+app.use(express.json({ limit: '50mb' }));
+
+// Increase URL-encoded payload limit (useful for POST and PUT requests)
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // Endpoint for Vercel
 app.get("/", (req, res) => res.send("Express on Vercel"));
