bfacfbecc1cc60df914aec98b77ddf82c4f019d2
Twila [Hyper] Habab <twila.shayne.habab@outlook.com>
2024-03-27 17:35:02 +0000

    Moved utils

--- medicines/cache/util/getCachedPath.js
+++
@@ -1,9 +0,0 @@
-import { firestore } from "../../../config/config.js";
-
-export const getCachedPath = async (type, id) => {
-    const medsDoc = await firestore.collection("medicines").doc(id).get();
-    const medsData = medsDoc.data();
-    const cachedPath = medsData[type + "Path"];
-
-    return cachedPath;
-};  
--- medicines/cache/util/getUserMeds.js
+++
@@ -1,9 +0,0 @@
-import { firestore } from "../../../config/config.js";
-
-export const getUserMeds = async (user) => {
-    const userDoc = await firestore.collection("users").doc(user).get();
-    const userData = userDoc.data();
-    const userMedicines = userData.medicines || {};
-
-    return userMedicines;
-};
